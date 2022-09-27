da06583276a0054ec75db6de3b8c0beb4c9ed7de
Somhairle MacLeòid <samuel@macleod.space>
2022-09-27 18:23:59 +0100

    Create workerd.rb

---
+++ Formula/workerd.rb
@@ -0,0 +1,20 @@
+require "language/node"
+
+class Workerd < Formula
+  desc "👷 workerd, Cloudflare's JavaScript/Wasm Runtime"
+  homepage "https://github.com/cloudflare/workerd"
+  url "https://registry.npmjs.org/workerd/-/workerd-1.20220926.2.tgz"
+  sha256 "9deae9872f969914138949501379e6f610db8e45fa8c484e6cea2a7d502d278b"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_equal "workerd 2022-09-26\n", shell_output("PWD=$(pwd) #{bin}/workerd --version")
+  end
+end
